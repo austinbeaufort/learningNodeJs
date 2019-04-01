463ea770d15af88ed24ed89a77dd2dfbcdb73ae2
Austin Beaufort <tinyhatcoding@gmail.com>
2019-04-01 19:24:41 -0400

    writing a server without framework

---
+++ CompleteGuideNodeJS/createServer/notes.js
@@ -0,0 +1,21 @@
+// CORE MODULES NODE SHIPS WITH
+
+// http => launch a server, send requests
+// https => launch a SLL server
+// fs 
+// path
+// os
+
+//  require('./ ') will ALWAYS look for a local path to a file.
+// require (' ') will ALWAYS look for a global module.
+
+// http.createServer(function(req, res) {
+
+//});
+
+// server.listen(3000);
+
+// EVENT LOOP CONTINUES TO RUN AS LONG AS..
+// THERE ARE EVENT LISTENERS REGISTERED
+
+// PROCESS.EXIT() DOES A "HARD EXIT" OF THE EVENT LOOP
---
+++ CompleteGuideNodeJS/notes2.js
@@ -0,0 +1,10 @@
+// HOW THE WEB WORKS
+
+// HTTP, HTTPS
+
+// HYPER TEXT TRANSFER PROTOCOL
+// a protocol for transferring data which 
+// is understood by browser and server.
+
+// HYPER TEXT TRANSFER PROTOCOL Secure
+// HTTP + data encryption (during transmission)
