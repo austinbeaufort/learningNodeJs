eda007e64f4ba3178bc551a4ccfadf98cf12cf03
Austin Beaufort <tinyhatcoding@gmail.com>
2019-04-02 18:07:34 -0400

    Assignment2 Complete, creating Express Server

---
+++ CompleteGuideNodeJS/Assignment2Express/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/users', (req, res, next) => {
+    console.log('funnel Two');
+    res.send('<h1> Users Page </h1>');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1> Welcome to my Page! </h1>');
+});
+
+
+
+app.listen(3000);
--- CompleteGuideNodeJS/expressJs/app.js
+++ CompleteGuideNodeJS/expressJs/app.js
@@ -1,8 +1,22 @@
-const http = require('http');
-
 const express = require('express');
 
+const app = express();
 
-const server = http.createServer();
+app.use('/', (req, res, next) => {
+    console.log('this always runs');
+    next();
+});
 
-server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+    console.log('in another middleware');
+    res.send('<h1>The "Add Product" Page</h1>');
+});
+
+
+app.use('/', (req, res, next) => {
+    console.log('in another middleware');
+    res.send('<h1>hello from express</h1>');
+});
+
+
+app.listen(3000);
