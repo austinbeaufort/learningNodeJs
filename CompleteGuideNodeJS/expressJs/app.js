9dfbd03425c389a972a6d7a1bc98773a4bfbd2f2
Austin Beaufort <tinyhatcoding@gmail.com>
2019-04-10 16:41:51 -0400

    express

--- CompleteGuideNodeJS/expressJs/app.js
+++ CompleteGuideNodeJS/expressJs/app.js
@@ -1,22 +1,20 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('this always runs');
-    next();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
-app.use('/add-product', (req, res, next) => {
-    console.log('in another middleware');
-    res.send('<h1>The "Add Product" Page</h1>');
-});
-
-
-app.use('/', (req, res, next) => {
-    console.log('in another middleware');
-    res.send('<h1>hello from express</h1>');
-});
-
 
 app.listen(3000);
